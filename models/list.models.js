0a9fd16f455537869263c2c94a78f899bf31309a
Courtney Marasigan <coumardev@gmail.com>
2022-11-30 12:09:57 -0500

    viewing individual lists

--- models/list.models.js
+++ models/list.models.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const listSchema = new Schema({
     title: {type: String},
     content: {type: [String]},
+    user: {type: Schema.Types.ObjectId, ref: "User"}
 });
 
 const List = mongoose.model('List', listSchema);
--- routes/list.routes.js
+++ routes/list.routes.js
@@ -15,14 +15,15 @@ router.get('/add-list', (req, res, next) => {
 
 router.post('/add-list', (req, res, next) => {
     // console.log("Checking values:", req.body.title, req.body.content)
-    if(!req.body.title || !req.body.content || !req.body.content.join('')){
+    if(!req.body.title || !req.body.content || (Array.isArray(req.body.content) && !req.body.content.join(''))){
         res.render('list-views/add-list.hbs', {message: 'please add a title and at least one list item before submitting'})
         return;
     }
     
     List.create({
         title: req.body.title,
-        content: req.body.content
+        content: req.body.content,
+        user: req.session.user._id
     })
     .then((createdList) => {
         console.log('MY NEW LIST', createdList)
@@ -34,7 +35,28 @@ router.post('/add-list', (req, res, next) => {
 });
 
 router.get('/all-lists', (req, res, next) => {
-    res.render('list-views/all-lists.hbs')
+    List.find({
+        user: req.session.user._id
+    })
+    .then((foundLists) => {
+        console.log(foundLists)
+        res.render('list-views/all-lists.hbs', {foundLists})
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 });
+
+router.get("/:id", (req, res, next) => {
+    List.findById(req.params.id)
+      .then((foundOneList) => {
+        console.log(foundOneList)
+        res.render('list-views/view-one.hbs', foundOneList);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
+  });
 
 module.exports = router;
